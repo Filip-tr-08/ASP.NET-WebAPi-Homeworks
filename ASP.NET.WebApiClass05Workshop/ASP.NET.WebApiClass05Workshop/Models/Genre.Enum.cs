377d2599e38fa3a33353100f6a7498f0faf85d38
Filip-tr-08 <filiptrajo@yahoo.com>
2021-09-23 11:00:40 +0200

    class05_Workshop

---
+++ ASP.NET.WebApiClass05Workshop/ASP.NET.WebApiClass05Workshop/Models/Genre.Enum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET.WebApiClass05Workshop.Models
+{
+    public enum Genre
+    {
+        Comedy=1,
+        Thriller,
+        Horror
+    }
+}
---
+++ ASP.NET.WebApiClass05Workshop/ASP.NET.WebApiClass05Workshop/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET.WebApiClass05Workshop.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int? Year { get; set; }
+        public Genre? Genre { get; set; }
+        public Director Director { get; set; }
+
+    }
+}
